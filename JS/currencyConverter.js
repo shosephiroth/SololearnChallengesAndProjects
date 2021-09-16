2eb4a966840022e2531f13160d0e08334a6bcb86
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-09-16 17:35:36 -0400

    finished? lolol
    I got that one right away!
    
    Currency Converter
    Run
    ProblemResults
    Test Case 1
    
    Test Case 2
    
    Test Case 3
    
    This test case is hidden
    
    Test Case 4
    
    This test case is hidden

--- JS/currencyConverter.js
+++ JS/currencyConverter.js
@@ -1,6 +1,11 @@
 function main() {
     var amount = parseFloat(readLine(), 10);
     var rate = parseFloat(readLine(), 10);
+
+    //create a function, take 2 parameters
+    function convert(amount, rate) {
+        return amount * rate;
+    }
     
     console.log(convert(amount, rate));
 }
