09c1ce1927af26e41c95415ad4ead1fc251ff0c0
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-09-15 20:38:47 -0400

    added variables
    added variables to (possibly) be used in loop.

--- theSnailInTheWell.js
+++ theSnailInTheWell.js
@@ -1,6 +1,9 @@
 function main() {
     var depth = parseInt(readLine(), 10);
     //your code goes here
+    let days = 0;
+    let feet = 0;
+    let distance = 32;
+    
     
 }
-// added notes in commit
