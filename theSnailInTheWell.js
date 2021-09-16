6ca4e6259a3fcb7c20c7a33beea343fd75f2cdb9
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-09-15 20:47:38 -0400

    Update theSnailInTheWell.js
    no dice (yet).

--- theSnailInTheWell.js
+++ theSnailInTheWell.js
@@ -4,6 +4,18 @@ function main() {
     let days = 0;
     let feet = 0;
     let distance = 32;
-    
+    while (feet <= distance) {
+        days++;
+        if (feet >= 32) {
+            feet += 7;
+            console.log(`days:${days} feet:${feet} distance:${distance}`);
+        }
+
+        if (feet >= 31) {
+            feet -=2;
+            console.log(`days:${days} feet:${feet} distance:${distance}`);
+        }
+    }
+
     
 }
