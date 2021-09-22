48822bff29b777b4e099a56f9678808303b53ee1
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-09-22 15:53:38 -0400

    complete!! (LC101 taught me well!!)
    Store Manager
    Run
    ProblemResults
    Test Case 1
    
    Input
    9
    Your Output
    [ 107.99, 24.2, 29, 1035 ]
    Expected Output
    [ 107.99, 24.2, 29, 1035 ]
    Test Case 2
    
    Input
    100
    Your Output
    [ 198.99, 115.2, 120, 1126 ]
    Expected Output
    [ 198.99, 115.2, 120, 1126 ]
    Test Case 3
    
    This test case is hidden
    
    Test Case 4
    
    This test case is hidden
    
    Test Case 5
    
    This test case is hidden

--- JS/storeManager.js
+++ JS/storeManager.js
@@ -2,5 +2,8 @@ function main() {
     var increase = parseInt(readLine(), 10);
     var prices = [98.99, 15.2, 20, 1026];
     //your code goes here
-    
+    for (let i = 0; i < prices.length; i++) {
+        prices[i] += increase;
+    }
+    console.log(prices);
 }
